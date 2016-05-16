e5f27fd40b6cd057981e947824c605019bc4858d
Kathy Dieser <katdiese@gmail.com>
2016-05-16 14:08:00 -0600

    functions to convert to binary, convert back to character

--- convert.js
+++ convert.js
@@ -3,7 +3,14 @@ function charToBinary(char) {
 }
 
 function binaryToChar(binary) {
-
+  var currChar = parseInt(binary,2).toString(10);
+  var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+  for(i = 0; i < alphabet.length - 1; i++) {
+    if(alphabet[i].charCodeAt(0) ==currChar ) {
+      console.log("This is " + alphabet[i]);
+    }
+  }
 }
 
-// charToBinary('B');
+charToBinary('A');
+binaryToChar(1000001);
